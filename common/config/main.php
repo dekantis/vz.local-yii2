80cb14d7db8aaeda3d7f2933c0d0af70c8b5a640
Nikita Osmakovskiy <dekantis@gmail.com>
2023-09-28 18:25:48 +0000

    added custom module

--- common/config/main.php
+++ common/config/main.php
@@ -4,9 +4,18 @@ return [
     'language' => 'ru',
     'charset'=>'utf-8',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'modules' => [
+        'example' => [
+            'class' => 'modules\example\Module',
+        ],
+    ],
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
     ],
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ]
 ];
